2104dbe07782a1134f3494fc00395c28bb3cc096
whoami <chao.chen@yoho.cn>
2020-03-22 18:51:37 +0800

    remove to another module

--- alc-backend/src/main/java/com/fcgo/weixin/convert/BaseConvert.java
+++
@@ -1,9 +0,0 @@
-package com.fcgo.weixin.convert;
-
-import com.fcgo.weixin.common.util.DateUtil;
-
-public class BaseConvert {
-    public static String fmtDateFromUnixtime(Integer datetime){
-        return DateUtil.getDateStrFromUnixTime(datetime,DateUtil.Format_yyyy_MM_dd_HH_mm_ss);
-    }
-}
